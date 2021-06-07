86e2f48c0eefa3cb06a18467aa6362ec783db32f
archi-Doc <archidoc422@gmail.com>
2021-06-08 00:26:22 +0900

    .

--- BasicTest/Program.cs
+++ BasicTest/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using ConsoleAppFramework;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace BasicTest
 {
@@ -17,9 +19,22 @@ namespace BasicTest
         public async Task Run(
             [Option("p", "The local port number to receive packets.")] int port)
         {
+            // Logger: Debug, Information, Warning, Error, Fatal
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.File(
+               /* Path.Combine("logs", "log.txt")*/"/logs/log.txt",
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 31,
+                buffered: true,
+                flushToDiskInterval: TimeSpan.FromMilliseconds(1000))
+            .CreateLogger();
+
+            Log.Information("test");
+
             Console.WriteLine($"port: {port}");
 
-            await Task.Delay(10000, this.Context.CancellationToken);
+            await Task.Delay(3000, this.Context.CancellationToken);
 
             Console.WriteLine("fin.");
         }
