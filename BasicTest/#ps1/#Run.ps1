01461dbc7406585a69730fc7173e4eae1f6967c4
archi-Doc <archidoc422@gmail.com>
2021-06-08 00:30:42 +0900

    .

--- BasicTest/#ps1/#Run.ps1
+++ BasicTest/#ps1/#Run.ps1
@@ -1,7 +1,7 @@
 # Set-ExecutionPolicy RemoteSigned
 
 # Run
-docker run --mount type=bind,source=c:/app/logvolume,destination=/logs --rm -p 49152:49152/udp archidoc422/netsphere-basictest -port 49152
+docker run --mount type=bind,source=$pwd/logvolume,destination=/logs --rm -p 49152:49152/udp archidoc422/netsphere-basictest -port 49152
 Write-Output ""
 
 Write-Output "" "Press any key to exit."
