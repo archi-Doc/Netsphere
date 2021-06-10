5f1706399c35aafad571ac5c6c8d9497d2f8fea0
archi-Doc <archidoc422@gmail.com>
2021-06-10 10:55:43 +0900

    .

--- BasicTest/#ps1/#Run.ps1
+++ BasicTest/#ps1/#Run.ps1
@@ -1,7 +1,8 @@
 # Set-ExecutionPolicy RemoteSigned
 
 # Run
-docker run --mount type=bind,source=$(pwd)/.app,destination=/app --rm -p 49152:49152/udp archidoc422/netsphere-basictest -dir /app -port 49152
+docker run --mount type=bind,source=$(pwd)/.app,destination=/app --rm -p 49152:49152/udp archidoc422/netsphere-basictest `
+-mode timer -dir /app -port 49152
 Write-Output ""
 
 Write-Output "" "Press any key to exit."
--- BasicTest/Program.cs
+++ BasicTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using ConsoleAppFramework;
@@ -9,8 +11,21 @@ using Serilog;
 
 namespace BasicTest
 {
+    internal class Options
+    {
+        public int Port { get; set; }
+
+        public bool Receiver { get; set; }
+    }
+
     internal class Program : ConsoleAppBase
     {
+        private Dictionary<string, Func<Options, Task>> modeToFunc = new()
+        {
+            { "transfer", Transfer },
+            { "timer", Timer },
+        };
+
         public static async Task Main(string[] args)
         {
             await Host.CreateDefaultBuilder().RunConsoleAppFrameworkAsync<Program>(args);
@@ -19,18 +34,68 @@ namespace BasicTest
         public async Task Run(
             [Option("local port number to transfer packets")] int port,
             [Option("true if the node is receiver")] bool receiver = false,
-            [Option("mode(transfer)")] string mode = "transfer",
+            [Option("mode(transfer)")] string mode = "timer",
             [Option("base directory for storing application data")] string dir = "")
         {
+            var options = new Options()
+            {
+                Port = port,
+                Receiver = receiver,
+            };
+
             this.InitializeLogger(dir);
 
-            Log.Information("start");
-
-            Console.WriteLine($"port: {port}");
-
-            await Task.Delay(2000, this.Context.CancellationToken);
+            if (this.modeToFunc.TryGetValue(mode, out var action))
+            {
+                Log.Information($"mode: {mode}");
+                await action(options);
+            }
+            else
+            {
+                Log.Error($"mode: {mode} not found.");
+            }
 
             Log.Information("fin");
+            Log.CloseAndFlush();
+        }
+
+        private static async Task Timer(Options option)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            Log.Information($"ticks: {sw.ElapsedTicks:#,0}");
+            Log.Information($"high resolution: {Stopwatch.IsHighResolution}");
+            Log.Information($"frequency: {Stopwatch.Frequency:#,0}");
+
+            var et = sw.ElapsedTicks;
+            var et2 = sw.ElapsedTicks;
+            Log.Information($"ticks: {et:#,0}");
+            Log.Information($"ticks: {et2:#,0}");
+
+            Log.Information($"ticks: {sw.ElapsedTicks:#,0}");
+            Log.Information($"ticks: {sw.ElapsedTicks:#,0}");
+            Log.Information($"ticks: {sw.ElapsedTicks:#,0}");
+
+            Log.Information($"delay: 0");
+            Log.Information($"ticks: {sw.ElapsedTicks:#,0}");
+
+            await Task.Delay(1);
+            Log.Information($"delay: 1");
+            Log.Information($"ticks: {sw.ElapsedTicks:#,0}");
+
+            await Task.Delay(10);
+            Log.Information($"delay: 10");
+            Log.Information($"ticks: {sw.ElapsedTicks:#,0}");
+
+            // await Task.Delay(2000, this.Context.CancellationToken);
+        }
+
+        private static async Task Transfer(Options option)
+        {
+            Log.Information($"port: {option.Port}");
+
+            // await Task.Delay(2000, this.Context.CancellationToken);
         }
 
         private void InitializeLogger(string dir)
@@ -38,6 +103,7 @@ namespace BasicTest
             // Logger: Debug, Information, Warning, Error, Fatal
             Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()
+            .WriteTo.Console()
             .WriteTo.File(
                 Path.Combine(dir, "logs", "log.txt"),
                 rollingInterval: RollingInterval.Day,
