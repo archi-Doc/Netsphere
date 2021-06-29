bb2abd1189b0d9e33fb11ed5d765565716b6c19e
archi-Doc <archidoc422@gmail.com>
2021-06-29 19:12:44 +0900

    .

--- Tests/BasicTest/ThreadCore.cs
+++ Tests/BasicTest/ThreadCore.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
