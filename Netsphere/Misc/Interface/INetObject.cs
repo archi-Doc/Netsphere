64a1fbcaf423a1267b9db42dec2cd4be01b28a6e
archi-Doc <archidoc422@gmail.com>
2026-03-03 14:29:01 +0900

    .

---
+++ Netsphere/Misc/Interface/INetObject.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace Netsphere;
+
+/// <summary>
+/// Represents a network object that can respond to connection closure events.
+/// </summary>
+public interface INetObject
+{
+    /// <summary>
+    /// Called when the network connection associated with this object is closed.
+    /// </summary>
+    void OnConnectionClosed();
+}
