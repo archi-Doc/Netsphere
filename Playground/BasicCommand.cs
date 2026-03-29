cb0111a8e7fbff377af47d4c8cd6a2f4f0970c3d
archi-Doc <archidoc422@gmail.com>
2026-03-30 08:46:14 +0900

    .

--- Playground/BasicCommand.cs
+++ Playground/BasicCommand.cs
@@ -47,7 +47,7 @@ public class BasicCommand : ISimpleCommandAsync<BasicCommandOptions>, IClockHand
         using (var connection = (await netTerminal.Connect(Alternative.NetNode))!)
         {
             var service = connection.GetService<ITestService>();
-            service.MethodB(2, new((result, value) => { Console.WriteLine(value); }));
+            service.MethodB(2, new(static (result, value) => { Console.WriteLine(value); }));
 
             await Task.Delay(100);
         }
